8a3eef75ed8016cdec465d908c29b298e6a00e76
Mari Good <goodmari@yahoo.com>
2020-04-15 15:25:09 -0700

    Added starting windows files

---
+++ MidtermPracticeWindows/BinaryNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
